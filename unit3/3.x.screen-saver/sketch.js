e56bba7c7e6f280a09297752019cf39348a484ae
rusul <rusulabbas16@yahoo.com>
2024-11-06 21:03:42 -0500

    Update sketch.js

--- unit3/3.x.screen-saver/sketch.js
+++ unit3/3.x.screen-saver/sketch.js
@@ -1,35 +1,36 @@
-// position and speed for the shapes
+// I added a variable to track interaction and inactivty. I know I used this code for another assignment but I thought it would
+// nice to itterate it becuase most screensaver have movement and the DVD screen saver is similar to what I created for the bounce exercise
+//this was hard and not my best work but I think I got the right idea from the assignment description 
+
 let x = 0;
 let y = 0;
-let xspeed = 7;
-let yspeed = 7;
+let xspeed = 2;
+let yspeed = 2;
 let z = 0;
 let o = 0;
-let zspeed = 8;
-let ospeed = 8;
+let zspeed = 2;
+let ospeed = 2;
 let a = 0;
 let r = 0;
 let s = 0;
-let rspeed = 9;
-let sspeed = 9;
+let rspeed = 2;
+let sspeed = 2;
 
 // inactivity timer variables
 let lastInteractionTime = 0;  // Variable to track when the last interaction happened
 let inactivityTimeout = 5000; // 5 seconds of inactivity before pausing the animation
 
+
+//size of Canvas
 function setup() {
   createCanvas(400, 300);
   fill('white');
 }
 
 function draw() {
-  // Check inactivity and pause animation
+  // Check inactivity and pause animation 
   if (millis() - lastInteractionTime > inactivityTimeout) {
-    background(0); // Set background to black when paused
-    fill(255); // Display a message
-    textSize(24);
-    textAlign(CENTER, CENTER);
-    text("Screensaver Paused", width / 2, height / 2);
+    background(250); // Set background to black when paused
     return; // Pause the animation here
   }
   
@@ -38,9 +39,12 @@ function draw() {
   noStroke();
   translate(width / 2, height / 2);
 
-  // Square logic
+    // checks where the square is 
+
   if (x > 175 || x < -175) {
-    xspeed = -xspeed; // Bounce the square
+    xspeed = -xspeed; // if the square has gone too far to the 
+    //left or right the opposite direction
+    //randomizes the color of the shape
     fill(random(255), 90, 90);
   }
   if (y > 125 || y < -125) {
@@ -49,7 +53,6 @@ function draw() {
   }
   square(x, y, 30);
 
-  // Second square logic
   if (r > 175 || r < -175) {
     rspeed = -rspeed;
     fill(random(255), 200, 200);
@@ -60,7 +63,6 @@ function draw() {
   }
   square(r, s, 20);
 
-  // Circle logic
   if (z > 175 || z < -175) {
     zspeed = -zspeed;
     fill(random(225), 100, 100);
@@ -71,8 +73,8 @@ function draw() {
   }
   circle(z, o, 10);
 
-  // Update positions
-  x += xspeed;
+// this updates the variables speed
+x += xspeed;
   y += yspeed;
   z += zspeed;
   o += ospeed;
@@ -89,23 +91,8 @@ function keyPressed() {
   lastInteractionTime = millis(); // Update the last interaction time
 }
 
-/* Links to resources used:
-
-- p5.js `millis()` function to track time:
-  - https://p5js.org/reference/#/p5/millis
-
-- p5.js `textSize()` function to control text size:
-  - https://p5js.org/reference/#/p5/textSize
-
-- p5.js `textAlign()` function to center text:
-  - https://p5js.org/reference/#/p5/textAlign
-
-- p5.js `fill()` function to set color:
-  - https://p5js.org/reference/#/p5/fill
-
-- p5.js `mouseMoved()` event function to detect mouse movement:
-  - https://p5js.org/reference/#/p5/mouseMoved
-
-- p5.js `keyPressed()` event function to detect key presses:
-  - https://p5js.org/reference/#/p5/keyPressed
-*/
+// Links to resources used:
+// https://p5js.org/reference/p5/millis/
+  //https://p5js.org/reference/p5/mouseMoved/
+  //https://p5js.org/reference/p5/keyPressed/
+//
