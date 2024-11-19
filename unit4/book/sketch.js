1a8ad99e379845adfe40a38e08027490b168323a
rusul <rusulabbas16@yahoo.com>
2024-11-18 21:27:37 -0500

    edits

--- unit4/book/sketch.js
+++ unit4/book/sketch.js
@@ -1,47 +1,38 @@
-let word = "abyss";  // changed the word to "abyss"
-let wc = 0;
+let word = "abyss";  // changed the word to abyss
+let wc = 0; 
 
 function setup() {
-
   noCanvas();
 
   let content = select("body");
   content.attribute("id", "content");
-
-  content.child(createElement("h1", "Many " + word + "s"));  
-  content.style("font-family",  'impact');  //added a font
   
+  content.child(createElement("h1", "The Many " + word + "s"));
 
-  while (wc < 50000) {
-    content.child(createElement("h2", String(word + " ").repeat(random(2, 6)).toUpperCase()));  
-    
-    for (let p = 0; p < random(5, 10); p++) {  // changed number of paragraphs randomly generated
+  while (wc < 50000) {  
+    content.child(createElement("h2", String(word + " ").repeat(random(2, 6)).toUpperCase()));
+
+    for (let p = 0; p < random(5, 20); p++) {  // changed number of paragraphs randomly generated
       let paragraph = '';
-      for (let s = 0; s < random(3, 10); s++) {  // changed number of sentences in each paragraph
+      for (let s = 0; s < random(3, 20); s++) {   // changed number of sentences in each paragraph
+        let internalPunct = [",", ",", ",", ";", " --"]; //added more to list of internal punctuation options
+        let endPunct = [".", ".", "?", "!"]; // added more to list of sentence-ending punctuation
+        let sentence = word.charAt(0).toUpperCase() + word.slice(1);
+        let sentenceLength = random(9, 25); // changed repetitions of "abyss" in the sentence
+        wc += sentenceLength + 1; // changed the sentence length
 
-        let internalPunct = [",", ",", ",", ";", " --", ":"];  //added more to list of internal punctuation options
-
-        let endPunct = [".", ".", "?", "!", "!!", "..."];  // added more to list of sentence-ending punctuation
-
-        let sentence = word.charAt(0).toUpperCase() + word.slice(1); 
-        sentence += " " + word.repeat(random(2, 5));  // changed repetitions of "abyss" in the sentence
-
-        let sentenceLength = random(5, 20);  // changed the sentence length
-
-        wc += sentenceLength;
-
-        for (let w = 0; w < sentenceLength; w++) {
-          sentence += " " + word;  
-          if (random() < 0.2) {  
-            sentence += random(internalPunct);
+        for (let w = 0; w < sentenceLength; w++) {  
+          sentence += " " + word;
+          if (random() < 0.2) {
+            sentence += random(internalPunct);  
           }
         }
         sentence += random(endPunct) + " ";  
-        paragraph += sentence;  
+        paragraph += sentence;
       }
-      content.child(createElement("p", paragraph));  // add the paragrah to the content 
+      content.child(createElement("p", paragraph));
+    }
   }
 
-  window.PagedPolyfill.preview();
-}
-}
+  
+  window.PagedPolyfill.preview();}
