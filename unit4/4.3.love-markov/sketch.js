0d96450497c164a7fec71e6dc48d140575bdd448
rusul <rusulabbas16@yahoo.com>
2024-11-21 23:45:10 -0500

    Edits

--- unit4/4.3.love-markov/sketch.js
+++ unit4/4.3.love-markov/sketch.js
@@ -1,14 +1,38 @@
+// Creates the depth of the Markow 
+let rm = RiTa.markov(4);
 
+//loading the text from the two books I selected
+let data = document.getElementById("source").innerText;
 
-let rm = RiTa.markov(2);
+// Adds the data
+rm.addText(data);
 
+function setup() {
+    // generates 6 lines of text 
+    let lines = rm.generate(6);
 
+// find the element to add to
+    let letter = select("#letter"); 
 
-function preload() {
-  result = loadStrings('');
+// add a salutation
+    letter.child( createP('My Dearest Harrington,'));
+
+// loop through the array of lines, adding each as a new p element
+    for (let l = 0; l < lines.length; l++){
+	let paragraph = createP(lines[l]);
+	letter.child(paragraph);
 }
 
-function setup(){
-
-	
+// add a closing
+    letter.child( createP('Sincerely,<br>Markov'));
 }
+
+//changing the letter everytime someone clicks their mosue
+function mousePressed() {
+    letter.remove();
+    letter = createDiv();
+    letter.id("letter"); 
+
+    setup();
+}
+
